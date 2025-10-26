821b7c2290ce7bfd0c496b94c4c7c52146f8140f
Sidonie Bouthors <sidonie.bouthors@epfl.ch>
2025-10-26 22:34:15 +0100

    pat: bindings

--- output/src/main/scala/fix/Parsed.scala
+++ output/src/main/scala/fix/Parsed.scala
@@ -1,6 +1,3 @@
-/*
-rule = ParsedRule
- */
 package fix
 
 object Parsed:
@@ -9,11 +6,11 @@ object Parsed:
 
   infix def +(b: Int): Int = this.hashCode() + b
 
-  val x = 1./(2)
-  val y = x.-(3)
-  val z = y.*(4)
-  val w = z.+(5)
-  val u = w./(6)
+  val x = 1 / 2
+  val y = x - 3
+  val z = y * 4
+  val w = z + 5
+  val u = w / 6
   val v = u - 7
   val t = v * 8
   val s = t + 9
--- rules/src/main/scala/fix/ParsedRule.scala
+++ rules/src/main/scala/fix/ParsedRule.scala
@@ -4,14 +4,7 @@ import scalafix.v1._
 import scala.meta._
 import scala.meta.dialects.Scala3
 import scala.collection.mutable.ListBuffer
-
-val rules: List[String] =
-  List(
-    """(?{_}).+(?{_})""",
-    """(?{_}).-(?{_})""",
-    """(?{_}).*(?{_})""",
-    """(?{_})./(?{_})"""
-  )
+import scala.io.Source._
 
 case class LintMessage(t: Tree, r: String) extends Diagnostic {
   override def position: Position = t.pos
@@ -20,68 +13,136 @@ case class LintMessage(t: Tree, r: String) extends Diagnostic {
 }
 
 class ParsedRule extends SemanticRule("ParsedRule"):
+
+  type Bindings = Map[String, Tree]
+  type MatchResult = Option[Bindings]
+
   override def fix(implicit doc: SemanticDocument): Patch =
 
+    val matchFile = sys.env.get("MATCH_RULE_CONF") match
+      case None           => ".matchrule.conf"
+      case Some(filename) => filename
+    val rewriteFile = sys.env.get("REWRITE_RULE_CONF") match
+      case None           => ".rewriterule.conf"
+      case Some(filename) => filename
+
+    val matchRules = fromFile(matchFile).getLines.toList
+    val rewriteRules = fromFile(rewriteFile).getLines.toList
+
     // Parse rules
-    val ruleTrees = rules.map(_.parse[Stat].get)
+    val matchRuleTrees = matchRules.map(_.parse[Stat].get)
+    val rewriteRuleTrees = rewriteRules.map(_.parse[Stat].get)
+
+    // Warn if number of match and rewrite rules differ
+    if matchRuleTrees.size != rewriteRuleTrees.size then
+      println(
+        s"Warning: Number of match rules (${matchRuleTrees.size}) " +
+          s"does not match number of rewrite rules (${rewriteRuleTrees.size})."
+      )
+    val ruleTrees = matchRuleTrees.zip(rewriteRuleTrees)
 
     val result = collectTopLevelMatches(
       doc.tree,
       { case t =>
-        ruleTrees.find(compareTrees(_, t)) match
-          case Some(r) =>
-            Patch.lint(LintMessage(t, r.syntax))
-          case None =>
-            Patch.empty
+        ruleTrees
+          .flatMap { case (m, r) =>
+            compareTrees(m, t, Map.empty[String, Tree]).map { bindings =>
+              val rewrittenTree = applyBindings(r, bindings)
+              Patch.replaceTree(t, rewrittenTree.syntax)
+            }
+          }
+          .headOption
+          .getOrElse(Patch.empty)
+
       }
     )
 
     result.asPatch
 
-  def compareTrees(pattern: Tree, candidate: Tree): Boolean =
+  def compareTrees(
+      pattern: Tree,
+      candidate: Tree,
+      bindings: Bindings
+  ): MatchResult =
     pattern match
       // Special handling for particular constructs
       case Term.Apply(Term.Name("?"), List(Term.Block(List(arg)))) =>
-        matchWithPattern(arg, candidate)
+        matchWithPattern(arg, candidate, bindings)
       case Term.AnonymousFunction(
             Term.Apply(Term.Name("?"), List(Term.Block(List(arg))))
           ) =>
-        matchWithPattern(arg, candidate)
+        matchWithPattern(arg, candidate, bindings)
+      // General case
       case _ =>
         val prodStruc =
           pattern.productPrefix == candidate.productPrefix &&
             pattern.productArity == candidate.productArity
-        prodStruc &&
-        pattern.productIterator
-          .zip(candidate.productIterator)
-          .forall({ case (p, c) => compareFields(p, c) })
 
-  def compareFields(pat: Any, cand: Any): Boolean =
+        if (prodStruc) then
+          pattern.productIterator
+            .zip(candidate.productIterator)
+            .foldLeft[MatchResult](Some(bindings)) {
+              case (None, _) => None
+              case (Some(b), (p, c)) =>
+                compareFields(
+                  p,
+                  c,
+                  b
+                )
+            }
+        else None
+
+  def compareFields(pat: Any, cand: Any, bindings: Bindings): MatchResult =
     (pat, cand) match
       // Trees
-      case (p: Tree, c: Tree) => compareTrees(p, c)
+      case (p: Tree, c: Tree) => compareTrees(p, c, bindings)
       // Iterables
       case (p: Iterable[_], c: Iterable[_]) =>
         if p.size == c.size then
-          val checks = p.zip(c).map { case (pp, cp) => compareFields(pp, cp) }
-          checks.forall(identity)
-        else false
+          p.zip(c).foldLeft[MatchResult](Some(bindings)) {
+            case (None, _)           => None
+            case (Some(b), (pp, cp)) => compareFields(pp, cp, b)
+          }
+        else None
       // Other fields
-      case _ => pat == cand
+      case _ => if pat == cand then Some(bindings) else None
 
-  def matchWithPattern(pat: Tree, candidate: Tree): Boolean =
+  def matchWithPattern(
+      pat: Tree,
+      candidate: Tree,
+      bindings: Bindings
+  ): MatchResult =
     pat match
       case Term.ApplyUnary(Term.Name("+"), arg) =>
-        compareTrees(arg, candidate)
+        compareTrees(arg, candidate, bindings)
       case Term.ApplyInfix(a, Term.Name("|"), Nil, List(b: Tree)) =>
-        val acheck = matchWithPattern(a, candidate)
-        val bcheck = matchWithPattern(b, candidate)
-        acheck || bcheck
-      case Term.Placeholder() => true
+        matchWithPattern(a, candidate, bindings) match
+          case s @ Some(_) => s
+          case None        => matchWithPattern(b, candidate, bindings)
+      case Term.Placeholder() => Some(bindings)
       case Term.AnonymousFunction(f) =>
-        matchWithPattern(f, candidate)
+        matchWithPattern(f, candidate, bindings)
+      case Term.ApplyInfix(
+            Term.Name(name),
+            Term.Name("->"),
+            Nil,
+            List(v: Tree)
+          ) =>
+        matchWithPattern(v, candidate, bindings).map { newBindings =>
+          newBindings.updated(name, candidate)
+        }
       case _ =>
         throw new Exception(s"Unsupported pattern: ${pat.syntax}")
+
+  def applyBindings(tree: Tree, bindings: Bindings): Tree =
+    tree.transform {
+      case Term.Apply(
+            Term.Name("?"),
+            List(Term.Block(List(Term.Name(name))))
+          ) =>
+        if bindings.contains(name) then bindings(name)
+        else throw new Exception(s"No binding found for name: $name")
+    }
 
   def collectTopLevelMatches(
       tree: Tree,

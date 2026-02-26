31264595fb31776ce7315cfd729677531337f6c5
Sidonie Bouthors <sidonie.bouthors@epfl.ch>
2026-02-26 14:10:23 +0100

    cleanup

--- extra/output/BasicIf.scala
+++
@@ -1,6 +0,0 @@
-package fix
-
-object BasicIf:
-  def example(): Boolean =
-    val a = {println("hello world"); true}
-    a
--- extra/output/UselessHelper.scala
+++
@@ -1,9 +0,0 @@
-package fix
-
-object UselessHelper:
-
-  def outerFunction(p1: Int, p2: String): Boolean =
-    if p1 > 0 then
-      if p2.nonEmpty then true
-      else false
-    else false
